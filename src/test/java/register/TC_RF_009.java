75a4b2d0743baa7fd2ea040aac240a2f70d016be
Chaitanylatake917 <Chaitanylatake917@gmail.com>
2026-06-17 09:18:47 +0530

    Automated TC_7, TC_8, TC_9

---
+++ src/test/java/register/TC_RF_009.java
@@ -0,0 +1,38 @@
+package register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_009 {
+
+	@Test
+	public void emailIsExistsValidation() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.findElement(By.xpath("//a[@class='dropdown-toggle']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		driver.findElement(By.id("input-firstname")).sendKeys("Chaitanya");
+		driver.findElement(By.id("input-lastname")).sendKeys("Latake");
+		driver.findElement(By.id("input-email")).sendKeys("amotoori1@gmail.com");
+		driver.findElement(By.id("input-telephone")).sendKeys("7020299142");
+		driver.findElement(By.id("input-password")).sendKeys("Abc@134");
+		driver.findElement(By.id("input-confirm")).sendKeys("Abc@134");
+		driver.findElement(By.xpath("//input[@name='agree']")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+
+		String warningMsg = "Warning: E-Mail Address is already registered!";
+
+		Assert.assertEquals(driver.findElement(By.xpath("//ul[@class='breadcrumb']/following-sibling::div")).getText(),
+				warningMsg);
+		driver.quit();
+
+	}
+}
